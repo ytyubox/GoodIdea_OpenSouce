81ac500db1ad9a33fb1f9b506d8fcb966b0af94a
Tsungyu Yu <ytyubox@gmail.com>
2019-04-02 14:48:14 +0800

    open xcode project in item, and backward

--- general/online.sh
+++ general/online.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+secs=1
+echo -e "GET http://google.com HTTP/1.0\n\n" | nc -G 3 google.com 80 > /dev/null 2>&1
 
-echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 > /dev/null 2>&1
-
-if [ $? -eq 0 ]; then
+if [ $? -eq 0 ] ; then
     echo "Online"
 else
     echo "Offline"
